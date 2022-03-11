0fcdad82b732e5bb8976f296a63c950343f4ef30
omi-cpu <obriggs53@gmail.com>
2022-03-11 15:49:32 +0100

    day1 band name generation

---
+++ day1.py
@@ -0,0 +1,2 @@
+print("Hello there, " + "Welcome to Omi's Band Name Genertaor Program")
+print("Your band name could be: " + input("What city did you grow up in? \n") + " " + input("What's the name of your pet? \n"))
